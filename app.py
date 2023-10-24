77962bea7979d93dc046efcfda4a1782f62f79a3
Kulbhushan Magdum <kulbhushanmagdum1718@outlook.com>
2023-10-24 13:53:42 +0530

    code added

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask,request,render_template,jsonify
+from flask import Flask,request,render_template,jsonify  #importing the falsk
 import json
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/')    #creating route(way) for welcome page or home page 
+def welcome():         # method or defination
+    return "welcome to falsk"   # for the message 
 
-def welcome():
-    return "welcome to falsk"
-
-@app.route('/cal',methods=["GET"])
+@app.route('/cal',methods=["GET"])  #route for calculator and also mehtod get as well   
 def math_operation():
     operation=request.json["operation"]
     number1=request.json["number1"]
     number2=request.json["number2"]
 
+    #all the operation and results 
     if operation=='add':
         results=int(number1)+int(number2)
     elif operation=='mul':
@@ -22,6 +22,6 @@ def math_operation():
     else:
         results=int(number1)-int(number2)
     return jsonify(results)
-
+    #for run the file to create the starting point 
 if __name__=='__main__':
     app.run()  # flask run 5000 code of program 
